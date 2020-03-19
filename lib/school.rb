a00efb0a6631862ae5f1b6fba625151cd8a06aa3
Domenico Cascone <dcascone84@gmail.com>
2020-03-19 02:38:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+require "pry"
+
+class School
+  attr_accessor :roster
+
+  def initialize(school_name)
+    @name = school_name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if @roster.include?(grade)
+      @roster[grade] << student
+    else
+      @roster[grade] = [] << student
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster
+    sorted = {}
+    binding.pry
+  end
+end
